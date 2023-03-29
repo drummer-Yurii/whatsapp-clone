7172b01414cb7aa72950aef821725592fd09ab8e
Yurii Rybachok <drummer2009@ukr.net>
2023-03-29 14:45:05 +0300

    ChatView styling

---
+++ src/views/ChatsView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="Messages" class="pt-1 z-0 overflow-auto fixed h-[calc(100vh-100px)] w-[420px]">
+       
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
